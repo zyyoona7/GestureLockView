f4f57d1788874c43e5e92cd5a0de42642b7a9b70
zhangzhuoyue <19671746@qq.com>
2015-12-21 18:40:25 +0800

    demo中加入倒计时

---
+++ app/src/main/java/com/yoona/sample/DownTimer.java
@@ -0,0 +1,137 @@
+package com.yoona.sample;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+
+/**
+ * DownTimer
+ *
+ * @author cjj
+ * @version 1.0
+ * @category 倒计时工具类
+ * @content <b>1.实例化后必须设置倒计时的总时间（totalTime）和每隔多久时间（intervalTime）回调</b><br>
+ * <b>2.有start()、 cancel()、 pause()、resume() 四个方法看方法就知道它的意思了 </b>
+ */
+public class DownTimer {
+    private long totalTime = -1;
+    private long intervalTime = 0;
+    private long remainTime;
+    private long systemAddTotalTime;
+    private static final int TIME = 1;
+    private TimeListener listener;
+    private long curReminTime;
+    private boolean isPause = false;
+
+    public DownTimer() {
+    }
+
+    public void setIntervalTime(long intervalTime) {
+        this.intervalTime = intervalTime;
+    }
+
+    public void setTotalTime(long totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public long getIntervalTime() {
+        return intervalTime;
+    }
+
+    public long getTotalTime() {
+        return totalTime;
+    }
+
+    public void start() {
+        if (totalTime <= 0 && intervalTime <= 0) {
+            throw new RuntimeException("you must set the totalTime > 0 or intervalTime >0");
+        }
+
+        systemAddTotalTime = SystemClock.elapsedRealtime() + totalTime;
+
+        if (null != mHandler)
+            mHandler.sendEmptyMessage(TIME);
+    }
+
+    public void cancel() {
+        if (mHandler != null) {
+            mHandler.removeMessages(TIME);
+            mHandler = null;
+        }
+
+    }
+
+    public void pause() {
+        if (mHandler != null) {
+            mHandler.removeMessages(TIME);
+            isPause = true;
+            curReminTime = remainTime;
+        }
+
+    }
+
+    public void resume() {
+        if (isPause == true) {
+            isPause = false;
+            totalTime = curReminTime;
+            start();
+        }
+
+    }
+
+    private Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case TIME:
+                    if (!isPause)
+                        soloveTime();
+                    break;
+                case 2:
+                    isPause = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+
+    private void soloveTime() {
+        remainTime = systemAddTotalTime - SystemClock.elapsedRealtime();
+        if (remainTime <= 0) {
+            if (listener != null) {
+                listener.onFinish();
+                cancel();
+            }
+        } else if (remainTime < intervalTime) {
+            if (null != mHandler)
+                mHandler.sendEmptyMessageDelayed(TIME, remainTime);
+        } else {
+            long curSystemTime = SystemClock.elapsedRealtime();
+            if (listener != null) {
+                listener.onInterval(remainTime);
+            }
+
+            long delay = curSystemTime + intervalTime - SystemClock.elapsedRealtime();
+
+            while (delay < 0) delay += intervalTime;
+
+            if (null != mHandler) {
+                mHandler.sendEmptyMessageDelayed(TIME, delay);
+            }
+        }
+    }
+
+    public interface TimeListener {
+        public void onFinish();
+
+        public void onInterval(long remainTime);
+    }
+
+    public void setTimerLiener(TimeListener listener) {
+        this.listener = listener;
+    }
+
+}
--- app/src/main/java/com/yoona/sample/GestureUnLockActivity.java
+++ app/src/main/java/com/yoona/sample/GestureUnLockActivity.java
@@ -17,6 +17,8 @@ import java.util.Arrays;
 public class GestureUnLockActivity extends AppCompatActivity {
     private GestureLockViewGroup mGestureLockViewGroup;
     private TextView mTextView;
+    private DownTimer timer;
+    private GestureLockViewGroup.OnGestureLockViewListener mGestureLockViewListener;
     //    runnable去清除gestureLockView
     private final Runnable clearRunnable = new Runnable() {
         @Override
@@ -35,10 +37,9 @@ public class GestureUnLockActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gesture_un_lock);
         mGestureLockViewGroup = (GestureLockViewGroup) findViewById(R.id.id_gestureLockViewGroup_1);
         mTextView = (TextView) findViewById(R.id.id_textView2);
-        mGestureLockViewGroup.setUnMatchExceedBoundary(5);
+        timer = new DownTimer();//实例化
         SharedPreferences sp = getSharedPreferences("GestureLockView", Activity.MODE_PRIVATE);
         String password = sp.getString("GestureLockView", "");
-        Log.e("password", "手势密码：" + password);
         if (TextUtils.isEmpty(password)) {
             mGestureLockViewGroup.setAnswer(new int[]{1, 2, 5, 8});
             mTextView.setText("默认密码是1，2，5，8");
@@ -46,55 +47,83 @@ public class GestureUnLockActivity extends AppCompatActivity {
             Log.e("format", Arrays.toString(CommonUtils.toIntArray(password)));
             mGestureLockViewGroup.setAnswer(CommonUtils.toIntArray(password));
         }
+        mGestureLockViewListener = new GestureLockViewGroup.OnGestureLockViewListener() {
+
+            @Override
+            public void onUnmatchedExceedBoundary() {
+//                        Toast.makeText(MainActivity.this, "错误5次...",
+//                                Toast.LENGTH_SHORT).show();
+//                        mGestureLockViewGroup.setUnMatchExceedBoundary(5);
+                saveTime(System.currentTimeMillis());
+                mTextView.setText("30s后再试");
+                CommonUtils.startShakeAnim(GestureUnLockActivity.this, mTextView);
+                mGestureLockViewGroup.setTouchable(false);
+                //TODO 倒计时
+                setTimer(30 * 1000);
+                timer.start();
+            }
+
+            @Override
+            public void onGestureEvent(boolean matched, int tryTimes) {
+                postClearRunnable();
+//                        Toast.makeText(MainActivity.this, matched + "",
+//                                Toast.LENGTH_SHORT).show();
+                if (matched) {
+                    mTextView.setText("手势密码正确");
+                    mTextView.setTextColor(Color.parseColor("#00FF00"));
+                    finish();
+                } else {
+                    mTextView.setText("手势密码不正确");
+                    mTextView.setTextColor(Color.parseColor("#FF0000"));
+                    CommonUtils.startShakeAnim(GestureUnLockActivity.this, mTextView);
+                }
+                isFirstSelect = true;
+            }
+
+            @Override
+            public void onBlockSelected(int cId) {
+                if (isFirstSelect) {
+                    removeClearRunnable();
+                    isFirstSelect = false;
+                }
+            }
+        };
         SharedPreferences spTime = getSharedPreferences("Time", Activity.MODE_PRIVATE);
         long time = spTime.getLong("tryOutTime", -1);
         if (time == -1 || System.currentTimeMillis() - time > 30000) {
             mGestureLockViewGroup.setTouchable(true);
+            mGestureLockViewGroup.setUnMatchExceedBoundary(5);
             saveTime(-1);
             mGestureLockViewGroup
-                    .setOnGestureLockViewListener(new GestureLockViewGroup.OnGestureLockViewListener() {
-
-                        @Override
-                        public void onUnmatchedExceedBoundary() {
-//                        Toast.makeText(MainActivity.this, "错误5次...",
-//                                Toast.LENGTH_SHORT).show();
-//                        mGestureLockViewGroup.setUnMatchExceedBoundary(5);
-                            saveTime(System.currentTimeMillis());
-                            mTextView.setText("30s后再试");
-                            mTextView.setTextColor(Color.parseColor("#FF0000"));
-                            CommonUtils.startShakeAnim(GestureUnLockActivity.this, mTextView);
-                            mGestureLockViewGroup.setTouchable(false);
-                            //TODO 倒计时
-                        }
-
-                        @Override
-                        public void onGestureEvent(boolean matched, int tryTimes) {
-                            postClearRunnable();
-//                        Toast.makeText(MainActivity.this, matched + "",
-//                                Toast.LENGTH_SHORT).show();
-                            if (matched) {
-                                mTextView.setText("手势密码正确");
-                                mTextView.setTextColor(Color.parseColor("#00FF00"));
-                                finish();
-                            } else {
-                                mTextView.setText("手势密码不正确");
-                                mTextView.setTextColor(Color.parseColor("#FF0000"));
-                                CommonUtils.startShakeAnim(GestureUnLockActivity.this, mTextView);
-                            }
-                            isFirstSelect = true;
-                        }
-
-                        @Override
-                        public void onBlockSelected(int cId) {
-                            if (isFirstSelect) {
-                                removeClearRunnable();
-                                isFirstSelect = false;
-                            }
-                        }
-                    });
+                    .setOnGestureLockViewListener(mGestureLockViewListener);
         } else {
             mGestureLockViewGroup.setTouchable(false);
+            setTimer(30000 - (System.currentTimeMillis() - time));
+            timer.start();
         }
+
+
+    }
+
+    private void setTimer(long time) {
+        timer.setTotalTime(time);//设置毫秒数
+        timer.setIntervalTime(1000);//设置间隔数
+        timer.setTimerLiener(new DownTimer.TimeListener() {
+            @Override
+            public void onFinish() {
+                mGestureLockViewGroup.setTouchable(true);
+                mTextView.setText("绘制手势密码");
+                mTextView.setTextColor(Color.parseColor("#959595"));
+                mGestureLockViewGroup.setUnMatchExceedBoundary(5);
+                mGestureLockViewGroup.setOnGestureLockViewListener(mGestureLockViewListener);
+            }
+
+            @Override
+            public void onInterval(long remainTime) {
+                mTextView.setTextColor(Color.parseColor("#FF0000"));
+                mTextView.setText(remainTime / 1000 + "秒后继续");//剩余多少秒
+            }
+        });
     }
 
     private void saveTime(long time) {
@@ -113,6 +142,11 @@ public class GestureUnLockActivity extends AppCompatActivity {
         mGestureLockViewGroup.removeCallbacks(clearRunnable);
     }
 
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+    }
 }
 
 
